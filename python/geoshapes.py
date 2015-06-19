1a7f29195b15f5d1eae9a934b45f3bf88aa351eb
James Rising <jarising@gmail.com>
2015-06-19 17:08:21 -0400

    from office

---
+++ python/geoshapes.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+import shapefile
+from shapely.geometry import Polygon, MultiPolygon
+
+def shape2parts(shape):                                                     
+    parts = []                                                              
+    start_indexes = shape.parts                                             
+    prev_start_index = 0
+    for start_index in start_indexes[1:]:
+        parts.append(shape.points[prev_start_index:start_index])
+        prev_start_index = start_index
+    parts.append(shape.points[prev_start_index:])
+
+    return parts
+
+def shape2multi(shape):
+    parts = shape2parts(shape)
+    polygons = []
+    for part in parts:
+        polygons.append(Polygon(part))
+
+    return MultiPolygon(polygons)
+
+## To use, first writer as:
+# writer = shapefile.Writer(shapefile.POLYGON)
+# writer.autoBalance = 1
+## After all are written, save it:
+# writer.save('output/australia')
+def write_polys(writer, polygon, attribs=None):
+    if type(polygon) is Polygon:
+        writer.poly(shapeType=shapefile.POLYGON, parts=[polygon.exterior.coords])
+        if attribs is not None:
+            writer.record(*attribs)
+    else:
+        for geom in polygon.geoms:
+            writer.poly(shapeType=shapefile.POLYGON, parts=[geom.exterior.coords])
+            if attribs is not None:
+                writer.record(*attribs)
+
+def gcd_slc(long0, lat0, longs, lats):
+    R = 6371 # Earth mean radius [km]
+    long0 = long0 * np.pi / 180
+    lat0 = lat0 * np.pi / 180
+    longs = longs * np.pi / 180
+    lats = lats * np.pi / 180
+    
+    dists = np.arccos(np.sin(lat0)*np.sin(lats) + np.cos(lat0)*np.cos(lats) * np.cos(longs-long0)) * R
+    return dists # Distance in km
--- python/spacegrid.py
+++ python/spacegrid.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class SpatialGrid(object):
     def __init__(self, x0_corner, y0_corner, sizex, sizey, ncols, nrows):
         self.y0_corner = y0_corner
@@ -15,6 +17,14 @@ class SpatialGrid(object):
     def getll_raw(self, latitude, longitude):
         raise NotImplementedError()
 
+    def get_longitudes(self):
+        return np.arange(self.x0_corner + self.sizex / 2, self.x0_corner + self.sizex * self.ncols,
+                         step=self.sizex)
+
+    def get_latitudes(self):
+        return np.arange(self.y0_corner + self.sizey * self.nrows, self.y0_corner + self.sizey / 2,
+                         step=-self.sizey)
+
 class DelimitedSpatialGrid(SpatialGrid):
     def __init__(self, fp, x0_corner, y0_corner, sizex, sizey, ncols, nrows, delimiter):
         """Note that delimiter can be None, in which case multiple whitespace characters are the delimiter."""
@@ -29,3 +39,28 @@ class DelimitedSpatialGrid(SpatialGrid):
     def getll_raw(self, latitude, longitude):
         row, col = self.rowcol(latitude, longitude)
         return values[row, col]
+
+class AsciiSpatialGrid(DelimitedSpatialGrid):
+    def __init__(self, fp, delimiter):
+        count = 0
+        for line in fp:
+            vals = line.split(delimiter)
+            if vals[0] == 'ncols':
+                ncols = int(vals[1])
+            if vals[0] == 'nrows':
+                nrows = int(vals[1])
+            if vals[0] == 'xllcorner':
+                xllcorner = float(vals[1])
+            if vals[0] == 'yllcorner':
+                yllcorner = float(vals[1])
+            if vals[0] == 'cellsize':
+                cellsize = float(vals[1])
+            if vals[0] == 'NODATA_value':
+                nodata = float(vals[1])
+            count += 1
+            if count == 6:
+                break
+
+        super(AsciiSpatialGrid, self).__init__(fp, xllcorner, yllcorner, cellsize, cellsize, ncols, nrows, delimiter)
+        self.values[self.values == nodata] = np.nan
+
