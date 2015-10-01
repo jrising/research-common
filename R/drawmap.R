815d356cb8d2445f9a6b4c8c98d01bd489cd322a
James Rising <jarising@gmail.com>
2015-10-01 20:40:25 +0200

    changes for making gcp maps

--- R/drawmap.R
+++ R/drawmap.R
@@ -4,16 +4,16 @@ library(maps)
 library(RColorBrewer)
 library(classInt)
 
-draw.map <- function(database, names, values, colorscheme="RdYlGn", position=NA, add=F, digits=0) {
+draw.map <- function(database, names, values, colorscheme="RdYlGn", position=NA, add=F, digits=0, ylim=c(-90, 90), intervalStyle="quantile") {
     if (colorscheme == "GnYlRd")
         colors <- rev(brewer.pal(9, "RdYlGn"))
     else
         colors <- brewer.pal(9, colorscheme)
 
-    brks <- classIntervals(values, n=9, style="quantile")
+    brks <- classIntervals(values, n=9, style=intervalStyle)
     brks <- brks$brks
 
-    map(database, names, col=colors[findInterval(values, brks, all.inside=TRUE)], fill=T, mar=c(1, 2, 3, 0), add=add)
+    map(database, names, col=colors[findInterval(values, brks, all.inside=TRUE)], fill=T, mar=c(1, 2, 3, 0), add=add, ylim=ylim)
 
     if (!is.na(position))
         legend(position, legend=leglabs(round(brks, digits=digits)), fill=colors, bty="n", cex=.5)
--- python/geoshapes.py
+++ python/geoshapes.py
@@ -3,9 +3,9 @@ import numpy as np
 import shapefile
 from shapely.geometry import Polygon, MultiPolygon
 
-def shape2parts(shape):                                                     
-    parts = []                                                              
-    start_indexes = shape.parts                                             
+def shape2parts(shape):
+    parts = []
+    start_indexes = shape.parts
     prev_start_index = 0
     for start_index in start_indexes[1:]:
         parts.append(shape.points[prev_start_index:start_index])
@@ -33,6 +33,11 @@ def write_polys(writer, polygon, attribs=None):
         if attribs is not None:
             writer.record(*attribs)
     else:
+        writer.poly(shapeType=shapefile.POLYGON, parts=[geom.exterior.coords for geom in polygon.geoms])
+        if attribs is not None:
+            writer.record(*attribs)
+        return
+        # Delete below, if above works!
         for geom in polygon.geoms:
             writer.poly(shapeType=shapefile.POLYGON, parts=[geom.exterior.coords])
             if attribs is not None:
@@ -44,6 +49,6 @@ def gcd_slc(long0, lat0, longs, lats):
     lat0 = lat0 * np.pi / 180
     longs = longs * np.pi / 180
     lats = lats * np.pi / 180
-    
+
     dists = np.arccos(np.sin(lat0)*np.sin(lats) + np.cos(lat0)*np.cos(lats) * np.cos(longs-long0)) * R
     return dists # Distance in km
