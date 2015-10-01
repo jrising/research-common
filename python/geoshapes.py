0b2324669a3051f841131af05577ca7c30747181
James Rising <jarising@gmail.com>
2015-10-01 20:40:58 +0200

    function from coffee

--- python/geoshapes.py
+++ python/geoshapes.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 import shapefile
-from shapely.geometry import Polygon, MultiPolygon
+from shapely.geometry import Polygon, MultiPolygon, Point
 
 def shape2parts(shape):
     parts = []
@@ -52,3 +52,25 @@ def gcd_slc(long0, lat0, longs, lats):
 
     dists = np.arccos(np.sin(lat0)*np.sin(lats) + np.cos(lat0)*np.cos(lats) * np.cos(longs-long0)) * R
     return dists # Distance in km
+
+def weighted_grid_within(multi, latitude, longitude, callback, shape=(1,)):
+    """Determines weighted sum of the values of callback, weighted by the
+hectares represented in each grid cell.  Can handle np.array returned from callback.
+Returns the weighted area, and the absolute area."""
+    validlon = (longitude >= multi.bounds[0]) & (longitude <= multi.bounds[2])
+    validlat = (latitude >= multi.bounds[1]) & (latitude <= multi.bounds[3])
+
+    lons_rad = longitude * np.pi / 180
+    lats_rad = latitude * np.pi / 180
+    dlon_rad = np.mean(np.diff(lons_rad))
+    dlat_rad = np.mean(np.diff(lats_rad))
+
+    totals = np.zeros(shape)
+    for rr in np.flatnonzero(validlat):
+        for cc in np.flatnonzero(validlon):
+            if multi.contains(Point(longitude[cc], latitude[rr])):
+                fraction = dlon_rad * abs(np.sin(lats_rad[rr] - dlat_rad / 2) - np.sin(lats_rad[rr] + dlat_rad / 2)) / (4 * np.pi) # fraction of a sphere
+                area = 5.10064472e10 * fraction # hectares
+                totals += area * callback(rr, cc)
+
+    return totals
