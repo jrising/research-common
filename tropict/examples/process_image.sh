f12f4a643b8170f4e530b79a8061660fe36c28aa
James Rising <jarising@gmail.com>
2015-11-09 19:44:04 -0800

    try again

--- tropict/examples/process_image.sh
+++ tropict/examples/process_image.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
 python ../splice_grid.py subjects/arabica-future.png arabica-futureb.png
--- tropict/examples/process_ncdf.sh
+++ tropict/examples/process_ncdf.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
 python ../splice_grid.py subjects/bio-2.nc4 bio-2b.nc4
