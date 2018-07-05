0f189dbe3de26b9022b3288cd9b673ea5b12805a
James Rising <jarising@gmail.com>
2018-07-05 15:10:31 +0100

    add state lines

--- R/ggmap.R
+++ R/ggmap.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(dplyr)
 
-gg.usmap <- function(values, fips, borders=NA) {
+gg.usmap <- function(values, fips, borders=NA, extra.polygon.aes=c()) {
     df <- data.frame(values, fips, borders)
     data(county.fips)
 
@@ -9,10 +9,21 @@ gg.usmap <- function(values, fips, borders=NA) {
     map.county$polyname <- paste(map.county$region, map.county$subregion, sep=',')
     counties <- map.county %>% left_join(county.fips) %>% left_join(df)
 
-    if (class(borders) == "logical")
-        ggplot(counties, aes(x=long, y=lat, group=group, fill=values)) +
-            geom_polygon() + coord_map() + theme_minimal() + xlab(NULL) + ylab(NULL)
-    else
-        ggplot(counties, aes(x=long, y=lat, group=group, fill=values, colour=borders)) +
-            geom_polygon() + coord_map() + theme_minimal() + xlab(NULL) + ylab(NULL)
+    map.state <- map_data('state')
+    
+    if (class(borders) == "logical") {
+        polygon.aes <- c(aes(fill=values), extra.polygon.aes)
+        class(polygon.aes) <- "uneval"
+        ggplot(counties, aes(x=long, y=lat, group=group)) +
+            geom_polygon(polygon.aes) +
+            coord_map() + theme_minimal() + xlab(NULL) + ylab(NULL) +
+            geom_polygon(data=map.state, fill="#00000000", colour="#FFFFFF80", size=.3)
+    } else {
+        polygon.aes <- c(aes(fill=values, colour=borders), extra.polygon.aes)
+        class(polygon.aes) <- "uneval"
+        ggplot(counties, aes(x=long, y=lat, group=group)) +
+            geom_polygon(polygon.aes) +
+            coord_map() + theme_minimal() + xlab(NULL) + ylab(NULL) +
+            geom_polygon(data=map.state, fill="#00000000", colour="#FFFFFF80", size=.3)
+    }
 }
