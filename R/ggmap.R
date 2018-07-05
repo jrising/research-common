5aedaff863bc05af194dace24640449f13606f95
James Rising <jarising@gmail.com>
2018-07-05 14:20:06 +0100

    allow borders

--- R/ggmap.R
+++ R/ggmap.R
@@ -1,14 +1,18 @@
 library(ggplot2)
 library(dplyr)
 
-gg.usmap <- function(values, fips) {
-    df <- data.frame(values, fips)
+gg.usmap <- function(values, fips, borders=NA) {
+    df <- data.frame(values, fips, borders)
     data(county.fips)
 
     map.county <- map_data('county')
     map.county$polyname <- paste(map.county$region, map.county$subregion, sep=',')
     counties <- map.county %>% left_join(county.fips) %>% left_join(df)
 
-    ggplot(counties, aes(x=long, y=lat, group=group, fill=values)) +
-        geom_polygon() + coord_map() + theme_minimal() + xlab(NULL) + ylab(NULL)
+    if (class(borders) == "logical")
+        ggplot(counties, aes(x=long, y=lat, group=group, fill=values)) +
+            geom_polygon() + coord_map() + theme_minimal() + xlab(NULL) + ylab(NULL)
+    else
+        ggplot(counties, aes(x=long, y=lat, group=group, fill=values, colour=borders)) +
+            geom_polygon() + coord_map() + theme_minimal() + xlab(NULL) + ylab(NULL)
 }
